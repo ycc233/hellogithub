71002305af9f42ac957a17229cba5ace7519c0f3
ycc233 <2105861454@qq.com>
2024-04-15 16:46:40 +0800

    add a function plot_first_images

--- helloycc.py
+++ helloycc.py
@@ -1,2 +1,24 @@
 
+import numpy as np
+import matplotlib.pyplot as plt
 print("hello sbycc")
+
+
+#plot somethings of there
+#data
+def ycc_first_plot():
+    """
+    this is a demo for git flow, it would be 
+    plot a simple graph in current winodws
+    save in ycc_first_image.png
+    """
+
+    #data
+    x = np.arange(0,100,1)
+    y = np.sin(x)
+
+    plt.plot(x, y, '-', linewidth=0.8)
+    plt.show()
+    plt.savefig("ycc_first_image.png")    
+
+ycc_first_plot()
